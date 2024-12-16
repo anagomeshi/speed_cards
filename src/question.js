62e9c3f310ee0f2d3dd3ae5482bdaf7edc23761a
anagomeshi <121627718+anagomeshi@users.noreply.github.com>
2024-12-17 07:36:00 +0900

    Update question.js

--- src/question.js
+++ src/question.js
@@ -9,9 +9,9 @@ let cardSliderItemWidth = 0;
 
 function shuffleArray(array){
     for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1)); // 0 から i のランダムなインデックス
-    [array[i], array[j]] = [array[j], array[i]]; // 要素を入れ替えます
-  }
+      let j = Math.floor(Math.random() * (i + 1)); // 0 から i のランダムなインデックス
+      [array[i], array[j]] = [array[j], array[i]]; // 要素を入れ替えます
+    }
 }
 
 function generateQuestion(){
