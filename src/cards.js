3cac2ba2a8dde74c6b561c1e992e8c64d70989dc
anagomeshi <121627718+anagomeshi@users.noreply.github.com>
2024-12-19 12:24:27 +0900

    Update cards.js

--- src/cards.js
+++ src/cards.js
@@ -1,11 +1,17 @@
 const cardIndex = document.querySelector('.card-index');
 const cardList = document.querySelector('.card-list');
-const cardSlider = document.querySelector('.card-slider');
 
 const cardArray = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'H11', 'H12', 'H13'];
 
 let cardNumber = 52;
-let cardSliderItemWidth = 0;
+
+const swiper = new Swiper(".pickup-area", {
+    on: {
+        slideChange: function () {
+            cardIndex.innerText = `${swiper.realIndex + 1}/${cardNumber}`;
+        }
+      }
+});
 
 function shuffleArray(array){
     for (let i = array.length - 1; i > 0; i--) {
@@ -18,18 +24,14 @@ function shuffleArray(array){
 
 function generateQuestion(){
     const questionArray = shuffleArray([...cardArray]);
-    console.log(cardArray);
-    console.log(questionArray);
 
     cardList.innerHTML = '';
-    cardSlider.innerHTML = '';
+    swiper.removeAllSlides();
 
     for(let i = 0; i < cardNumber; i++){
         const newCardContainer = document.createElement('div');
         const newCardContainerSpan = document.createElement('span');
         const newCardContainerImg = document.createElement('img');
-        const newCardSliderLi = document.createElement('li');
-        const newCardSliderLiImg = document.createElement('img');
 
         newCardContainer.className = 'card-container';
         newCardContainerSpan.innerText = i + 1;
@@ -38,31 +40,14 @@ function generateQuestion(){
         newCardContainerImg.setAttribute('alt', '');
         newCardContainerImg.draggable = false;
 
-        newCardSliderLiImg.className = 'card';
-        newCardSliderLiImg.setAttribute('src', `icons/cards/${questionArray[i]}.svg`);
-        newCardSliderLiImg.setAttribute('alt', '');
-        newCardSliderLiImg.draggable = false;
-
         newCardContainer.appendChild(newCardContainerSpan);
         newCardContainer.appendChild(newCardContainerImg);
         cardList.appendChild(newCardContainer);
 
-        newCardSliderLi.appendChild(newCardSliderLiImg);
-        cardSlider.appendChild(newCardSliderLi);
+        swiper.appendSlide(`<div class="swiper-slide"><img class="card" src="icons/cards/${questionArray[i]}.svg" alt=""></div>`);
     }
-
-    cardSliderItemWidth = cardSlider.querySelectorAll('li')[0].offsetWidth;
 
     cardIndex.innerText = `1/${cardNumber}`;
 }
 
 generateQuestion();
-
-cardSlider.addEventListener("scroll", () => {
-    const scrollLeft = cardSlider.scrollLeft;
-
-    let visibleIndex = Math.round(scrollLeft / cardSliderItemWidth + 1);
-    visibleIndex = Math.max(1, Math.min(visibleIndex, cardNumber));
-
-    cardIndex.innerText = `${visibleIndex}/${cardNumber}`;
-});
