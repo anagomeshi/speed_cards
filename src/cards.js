3c0b62c67667a6000af5f8425deb19471e03b95c
anagomeshi <121627718+anagomeshi@users.noreply.github.com>
2024-12-23 19:54:25 +0900

    Update cards.js

--- src/cards.js
+++ src/cards.js
@@ -1,6 +1,7 @@
 const cardIndex = document.querySelector('.card-index');
 const cardList = document.querySelector('.card-list');
 const pickupArea = document.querySelector('.pickup-area');
+let pickupCard;
 
 const cardArray = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'H11', 'H12', 'H13'];
 let questionArray = [];
@@ -36,16 +37,7 @@ function changePickupCard(){
         currentIndex = cardNumber;
     }
 
-    const newPickupCard = document.createElement('img');
-
-    pickupArea.innerHTML = '';
-
-    newPickupCard.className = 'card';
-    newPickupCard.setAttribute('src', `icons/cards/${questionArray[currentIndex - 1]}.svg`);
-    newPickupCard.setAttribute('alt', '');
-    newPickupCard.draggable = false;
-
-    pickupArea.appendChild(newPickupCard);
+    pickupCard.src = `icons/cards/${questionArray[currentIndex - 1]}.svg`);
 
     cardIndex.innerText = `${currentIndex}/${cardNumber}`;
 }
@@ -79,6 +71,16 @@ function generateQuestion(){
     }
 
     // cardSlider.virtual.update(true);
+    const newPickupCard = document.createElement('img');
+
+    newPickupCard.className = 'card';
+    newPickupCard.setAttribute('alt', '');
+    newPickupCard.draggable = false;
+
+    pickupArea.appendChild(newPickupCard);
+
+    pickupCard = pickupArea.querySelector('.card');
+    
     changePickupCard();
 
     // cardIndex.innerText = `1/${cardNumber}`;
